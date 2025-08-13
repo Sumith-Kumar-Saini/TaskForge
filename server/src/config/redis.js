aa4000b9ba5e794ae4b6673846c533779b103c5e
Sumith Kumar Saini <sumithsaini01@gmail.com>
2025-08-13 17:22:10 +0530

    chore: improved redis reconnect log system

--- server/src/config/redis.js
+++ server/src/config/redis.js
@@ -13,8 +13,25 @@ const redis = new Redis({
   port: PORT,
   db: 0, // Use the appropriate database
   retryStrategy: (times) => {
-    const delay = Math.min(RETRY_DELAY_MS * Math.pow(2, times), MAX_RETRY_DELAY_MS);
-    logger.info(`Reconnecting to Redis... Attempt #${times + 1}, Delay: ${delay / 1000}s`);
+    const delay = Math.min(
+      RETRY_DELAY_MS * Math.pow(2, times),
+      MAX_RETRY_DELAY_MS
+    ); // Exponential backoff
+
+    if (times === MAX_RETRIES) {
+      logger.error(
+        `Max Redis retry attempts reached. Reconnecting to Redis failed. Attempt #${
+          times + 1
+        }, Delay: ${delay / 1000}s`
+      );
+    } else {
+      logger.debug(
+        `Reconnecting to Redis... Attempt #${times + 1}, Delay: ${
+          delay / 1000
+        }s`
+      );
+    }
+
     return delay; // Returns the delay time in ms for the next retry
   },
 });
@@ -25,10 +42,6 @@ redis.on("connect", () => {
 
 redis.on("error", (err) => {
   logger.error("Redis error:", err);
-});
-
-redis.on("end", () => {
-  logger.info("Redis connection closed");
 });
 
 export default redis;
